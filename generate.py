15a2e21f36553cdee7ecef5c9dc9256ab67721f2
Alex7833 <62905495+Alex7833@users.noreply.github.com>
2021-02-08 14:06:57 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,13 +6,15 @@ i = int(input("La taille du mot de passe : "))
 cliped = ""
 for loop in range(i):
     random_choice = [random.choice(string.ascii_letters), random.choice(string.digits), random.choice(string.punctuation)]
-    random_number = random.randint(0,2)
-    clip = random_choice[random_number]
+    test = random.randint(0,2)
+    clip = random_choice[test]
     cliped = cliped + clip
-
 print("Votre mot de passe : \n" + cliped + "\n")
-if input("Voulez vous copier le mot de passe au press-papier ? (Y/n):").upper() == "Y":
+
+string = input("Voulez vous copier le mot de passe au press-papier ? (Y/n):").upper()
+if string == "Y" or string == "":
     pyperclip.copy(cliped)
     print("Copié !\nFIN")
 else: 
     print("FIN")
+    exit()
